88b81d828fbd1b0332d1602fa99d13b9fb60cfb2
cdnicoll <cdnicoll@gmail.com>
2018-08-23 10:28:38 -0700

    Style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :update, :destroy, :show]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -9,45 +9,45 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def destroy
-    @article.destroy
-    flash[:notice] = "Article was destroyed successfully"
-    redirect_to articles_path
+  def edit
   end
 
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfully"
+      flash[:success] = "Article was created successfully"
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
-
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
     end
   end
 
+  def show
+  end
+
+  def destroy
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
